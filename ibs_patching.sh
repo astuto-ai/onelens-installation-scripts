9480639662bc84027ae45aeb2c640e41e116d1b6
manoj-astuto <manoj.v@astuto.ai>
2025-07-09 16:59:10 +0530

    Update ibs_patching.sh

--- ibs_patching.sh
+++ ibs_patching.sh
@@ -162,7 +162,7 @@ echo "helm upgrade onelens-agent onelens/onelens-agent with dynamic resource all
 helm repo add onelens https://astuto-ai.github.io/onelens-installation-scripts/
 helm repo update
 helm upgrade --install onelens-agent -n onelens-agent --create-namespace onelens/onelens-agent \
-    --version 0.1.1-beta.4 \
+    --version  \
     --reuse-values \
     --set prometheus.server.resources.requests.cpu="$PROMETHEUS_CPU_REQUEST'/" \
     --set prometheus.server.resources.requests.memory=$PROMETHEUS_MEMORY_REQUEST \
