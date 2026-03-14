c8bce1662a149048fc64598ae98a00a0a0365d55
Mangesh Devikar <mangesh@astuto.ai>
2026-03-15 02:21:22 +0530

    chore: bump versions to 2.1.16 for release (#148)

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ send_logs() {
 trap 'code=$?; if [ $code -ne 0 ]; then send_logs; fi; exit $code' EXIT
 
 # Phase 2: Environment Variable Setup
-: "${RELEASE_VERSION:=2.1.15}"
+: "${RELEASE_VERSION:=2.1.16}"
 : "${IMAGE_TAG:=v$RELEASE_VERSION}"
 : "${API_BASE_URL:=https://api-in.onelens.cloud}"
 : "${PVC_ENABLED:=true}"
@@ -440,7 +440,7 @@ if [ "$RELEASE_STATUS" = "failed" ]; then
 fi
 
 CMD="helm upgrade --install onelens-agent -n onelens-agent $CREATE_NS_FLAG onelens/onelens-agent \
-    --version \"\${RELEASE_VERSION:=2.1.15}\" \
+    --version \"\${RELEASE_VERSION:=2.1.16}\" \
     -f $FILE \
     --set onelens-agent.env.CLUSTER_NAME=\"$CLUSTER_NAME\" \
     --set-string onelens-agent.env.ACCOUNT_ID=\"$ACCOUNT\" \
